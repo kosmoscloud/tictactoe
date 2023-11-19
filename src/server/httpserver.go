bc8307cffe31f25564a0e08039d4c15b988dd3d1
Maciej Komosa <maciej.komosa.14@gmail.com>
2023-11-19 15:47:46 +0100

    dodanie gorilla router

--- src/server/httpserver.go
+++ src/server/httpserver.go
@@ -3,10 +3,16 @@ package server
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func InitServer() {
-	http.HandleFunc("/api/user", HandleUser)
+	userRouter := mux.NewRouter().PathPrefix("/api/user").Subrouter()
+	userRouter.HandleFunc("", HandleGetUser).Methods("GET")
+	userRouter.HandleFunc("", HandleCreateUser).Methods("POST")
+
+	http.Handle("/api/user", userRouter)
 }
 
 func Serve() {
